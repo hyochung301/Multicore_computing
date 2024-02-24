10c3ba39568f626247948f4cff68b38cc340d487
Sam Chung <hyochung@utexas.edu>
2024-02-24 15:30:16 -0600

    Lock_Queue LockFree_Stack

--- assignment3/q3/queue/LockQueue.java
+++ assignment3/q3/queue/LockQueue.java
@@ -1,20 +1,76 @@
 package queue;
 
+import java.util.EmptyStackException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockQueue implements MyQueue {
 // you are free to add members
+  ReentrantLock enqLock, deqLock;
+  Condition notEmpty;
+  Node head;
+  Node tail;
+  Node sentinel;
+  AtomicInteger count;
+
 
   public LockQueue() {
 	// implement your constructor here
+    sentinel = new Node(null);
+    tail = head = sentinel;  
+    enqLock = new ReentrantLock();
+    deqLock = new ReentrantLock();
+    notEmpty = deqLock.newCondition();
+    count = new AtomicInteger(0);
   }
   
   public boolean enq(Integer value) {
 	// implement your enq method here
-    return false;
+    if (value == null) {
+      throw new NullPointerException();
+    }
+    enqLock.lock();
+    try {
+      Node insertNode = new Node(value);
+      boolean wasEmpty = (head == tail); //if they are the same, the queue is empty
+      tail.next = insertNode;
+      tail = tail.next;
+      count.incrementAndGet();
+      if (wasEmpty) {
+        // if the queue was empty, signal notEmpty
+        deqLock.lock();
+        try {
+          notEmpty.signal();
+        } finally {
+          deqLock.unlock();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    } finally {
+      enqLock.unlock();
+    }
+    return true;
   }
   
   public Integer deq() {
 	// implement your deq method here
-    return null;
+    int result;
+    deqLock.lock();
+    try {
+      while (count.get() == 0) {
+        notEmpty.await();
+      }
+      result = head.next.value;
+      head = head.next;
+      count.decrementAndGet();
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      deqLock.unlock();
+    }
+    return result;
   }
   
   protected class Node {
--- assignment3/q3/stack/LockFreeStack.java
+++ assignment3/q3/stack/LockFreeStack.java
@@ -1,20 +1,47 @@
 package stack;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LockFreeStack implements MyStack {
 // you are free to add members
-	
+    AtomicReference<Node> top;
   public LockFreeStack() {
 	  // implement your constructor here
+      top = new AtomicReference<Node>(null);
   }
 	
   public boolean push(Integer value) {
 	  // implement your push method here
-	  return false;
+      Node node = new Node(value);
+      while (true) {
+          Node oldTop = top.get();
+          node.next = oldTop;
+          if (top.compareAndSet(oldTop, node)) {
+              return true;
+          }
+          else {
+              Thread.yield();
+          }
+      }
+      // return false;
   }
   
   public Integer pop() throws EmptyStack {
 	  // implement your pop method here
-	  return null;
+      while (true) {
+          Node oldTop = top.get();
+          if (oldTop == null) {
+              throw new EmptyStack();
+          }
+          Node newTop = oldTop.next;
+          if (top.compareAndSet(oldTop, newTop)) {
+              return oldTop.value;
+          }
+          else {
+              Thread.yield();
+          }
+      }
+	  // return null;
   }
   
   protected class Node {
