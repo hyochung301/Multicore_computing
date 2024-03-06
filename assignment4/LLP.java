4c75ac89557adcaf5ebfdf9e86256d47e31fc123
collebrusco <collebrusco.frank@gmail.com>
2024-03-05 21:07:29 -0600

    LLP api written but not tested

--- assignment4/LLP.java
+++ assignment4/LLP.java
@@ -1,9 +1,27 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.ArrayList;
+
 public abstract class LLP {
     // Feel free to add any methods here. Common parameters (e.g. number of processes)
     // can be passed up through a super constructor. Your code will be tested by creating
     // an instance of a sub-class, calling the solve() method below, and then calling the
     // sub-class's getSolution() method. You are free to modify anything else as long as
     // you follow this API (see SimpleTest.java)
+
+    private ExecutorService workers;
+    private ArrayList<ParallelAdvancer> advancers;
+
+    protected int[] G;
+    protected final int state_dimension;
+
+    public LLP(int sd) {
+        state_dimension = sd;
+        G = new int[sd];
+        advancers = new ArrayList<ParallelAdvancer>();
+        for (int i = 0; i < sd; i++) {advancers.add(new ParallelAdvancer(i));}
+        workers = Executors.newFixedThreadPool(state_dimension);
+    }
 
     // Checks whether process j is forbidden in the state vector G
     public abstract boolean forbidden(int j);
@@ -18,5 +36,36 @@ public abstract class LLP {
         // 2. Advance on forbidden states in parallel
         // 3. Repeat 1 and 2 until there are no forbidden states
 
+        while (true) {
+            ArrayList<Integer> forbidden = new ArrayList<Integer>();
+            for (int i = 0; i < state_dimension; i++) {
+                if (forbidden(i))
+                    forbidden.add(i);
+            }
+            if (forbidden.size() == 0) break;
+
+            for (Integer f : forbidden) {
+                workers.execute(advancers.get(f));
+            }
+        }
+
+        workers.shutdown();
+        while (!workers.isTerminated()) {}
+    }
+
+
+    /*
+        
+    */
+    private class ParallelAdvancer implements Runnable {
+        
+        private final int j;
+
+        public ParallelAdvancer(int i) {j = i;}
+
+        @Override
+        public void run() {
+            LLP.this.advance(j);
+        }
     }
 }
